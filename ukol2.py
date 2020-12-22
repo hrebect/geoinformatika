d1cfa97a91ec2e9a4af41356eaccd03fbbdaceba
Tomáš Hřebec <hrebect@natur.cuni.cz>
2020-12-22 15:48:04 +0100

    vybre kontejneru

--- ukol2.py
+++ ukol2.py
@@ -21,13 +21,29 @@ except FileNotFoundError:
 
 
 adresy_features = adresy_data["features"] 
+conteiner_features = kontejnery_data["features"]
+
+kontejnery = []
+#vyber souradnic kontejneru
+
+for conteiner in conteiner_features: #projde vsechny kontejnery
+     c_properties = conteiner["properties"]
+     c_pristup = c_properties["PRISTUP"]
+     
+     if c_pristup == 'volně': #souradnice volnych kontejneru ulozi do listu 'kontejnery'
+        c_geometry = conteiner["geometry"]
+        c_coordinates = (c_geometry['coordinates'])
+        kontejnery.append(c_coordinates)
+
 
 for buliding in adresy_features:  #projede vsechny budovy
-    properties = buliding["properties"]
-    geometry = buliding["geometry"]
-    coordinates = (geometry['coordinates'])
-    address = str(properties["addr:street"] + ' ' + properties["addr:housenumber"])
-    y,x = wgs2jtsk.transform(coordinates[1],coordinates[0])
+    b_properties = buliding["properties"]
+    b_geometry = buliding["geometry"]
+    b_coordinates = (b_geometry['coordinates'])
+    b_address = str(b_properties["addr:street"] + ' ' + b_properties["addr:housenumber"])
+    y,x = wgs2jtsk.transform(b_coordinates[1],b_coordinates[0]) #prevod souradnic
     coord_jtsk = [y,x] #ulozeni souradnic jako list, aby byly stejný typ jako souradnice kontejneru
-    print(coord_jtsk)
+    
+    
+    #print(coord_jtsk)
 
