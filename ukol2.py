775af92da526d171bda1ab1a9db137657a81b9dd
Tomáš Hřebec <hrebect@natur.cuni.cz>
2020-12-22 13:45:56 +0100

    ziskani adres

--- ukol2.py
+++ ukol2.py
@@ -1,8 +1,23 @@
-import json
+import json, sys
+
 
 #nacteni souboru
-with open('adresy.geojson',encoding='utf-8') as f:
-    kontejnery = json.load(f)
+try:
+    with open('adresy.geojson',encoding='utf-8') as f:
+        adresy_data = json.load(f)
+except FileNotFoundError:
+    sys.exit('Soubor s adresami nenalezen')
+try:
+    with open('kontejnery.geojson',encoding='utf-8') as e:   
+        kontejnery_data = json.load(e)
+except FileNotFoundError:
+    sys.exit('Soubor s kontejnery nenalezen')
 
 
-print(type(kontejnery))
+
+adresy_features = adresy_data["features"] 
+for buliding in adresy_features:  #projede vsechny budovy
+    
+    properties = buliding["properties"]
+    geometry = buliding["geometry"]
+    print(properties["addr:street"], properties["addr:housenumber"])
