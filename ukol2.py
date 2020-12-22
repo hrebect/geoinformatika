87c8c79dcbef91741a59762dcd2187beda2b1a51
Tomáš Hřebec <hrebect@natur.cuni.cz>
2020-12-22 11:38:01 +0100

    nacteni dat

--- ukol2.py
+++ ukol2.py
@@ -1,8 +1,8 @@
 import json
 
 #nacteni souboru
-with open('kontejnery.geojson', 'r') as f:
-    data=json.loads(f)
+with open('adresy.geojson',encoding='utf-8') as f:
+    kontejnery = json.load(f)
 
 
-print(type(data))
+print(type(kontejnery))
