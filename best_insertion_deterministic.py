43cca53cddecfb5b3e5f01fcbcb1567010f07cca
hrebect <74654372+hrebect@users.noreply.github.com>
2022-01-18 15:31:10 +0100

    Add files via upload

--- best_insertion_deterministic.py
+++ best_insertion_deterministic.py
@@ -2,18 +2,61 @@ from math import sqrt, inf
 import matplotlib.pyplot as plt
 import csv, random, copy
 
-def BestInsertion(V,C):
+def rand(V):
+    # initialize path with 3 random points
+    start = random.sample(range(len(V)),3)
+    return start
+
+def nearest(V,C,u):
+    # Initialize path with pount u and its 2 nearest neighbor
+    start = []
+    #Initialize state of nodes,
+    S = ['N'] * len(V)
+    start.append(u)
+    # Set starting point as open 
+    S[u] = 'O'
+    # Finde 2 nearest neighbor
+    for i in range(2):
+        # Initialize minimal distance between points as inf
+        minW = inf
+        for v in V:
+            # Get non-visited node as v
+            if S[v] == 'N':
+                # calculate distance between u and v
+                dist = sqrt((C[u][0]-C[v][0])**2 + (C[u][1]-C[v][1])**2)
+                # save minimum distance and nearest neighbor
+                if dist < minW:
+                    minW = dist
+                    nn = v
+        # Add nn to Initialize path          
+        start.append(nn)
+        S[nn] = 'O'
+    return start
+
+def extreme(C):
+    # Initialize path as extreme points
+    # get nodes with extreme coords
+    minx = min(C.items(), key=lambda x: x[1][0])[0]
+    miny = min(C.items(), key=lambda x: x[1][1])[0]
+    maxx = max(C.items(), key=lambda x: x[1][0])[0]
+    maxy = max(C.items(), key=lambda x: x[1][1])[0]
+    # save extreme nodes in correct order
+    start = [minx,maxy,maxx,miny]
+    return start
+
+def BestInsertion(V,C,a):
+    # Input V = list of nodes, C = dictionary of coords of nodes, a = initialization method
     # Initialize state of nodes, W of Hamiltonian path, Nodes of Hamiltonian path and queue of nodes
     S = ['N'] * len(V)
     W = 0
     Q = []
     V_copy = copy.copy(V)
-    # Choose 3 random nodes from queue of nodes as list
-    start = random.sample(range(len(V_copy)),3)
+    # Initialize starting path by chosen method
+    start = a
     #close initial Hamiltoninan path
     start.append(start[0])
     # Set 3 starting nodes as Open
-    for i in range(3):
+    for i in range(len(start)-1):
         S[start[i]] = 'O'
         # Calculate distnce W of startig path
         dist = sqrt((C[start[i]][0]-C[start[i+1]][0])**2 + (C[start[i]][1]-C[start[i+1]][1])**2)
@@ -82,6 +125,6 @@ with open('data//coord_cernosice.csv','r') as f:
         V.append(int(row[0]))
         C[int(row[0])] = [float(row[1]),float(row[2])]
 
-Q, W, start = BestInsertion(V,C)
-print(W, Q , start)
+Q, W, start = BestInsertion(V,C,nearest(V,C,20))
+print(W, Q, start)
 plot(Q, C)
